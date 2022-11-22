e3245b1d7972c5c1cb05fdd82a06c02cde825ff0
Richard CHEN <richard.chen32^gmail.com>
2022-11-22 15:18:08 +0100

    ajout de test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void getString(){
+        String msg = "Hellow";
+        assertEquals("Hellow",msg);
+    }
+}
